49ea0f0b2764cc392f0e85602699472b2a348a47
Rajnikanth1999 <praveendba31@gmail.com>
2022-12-05 10:57:35 +0530

    added kubernetes script

--- kubernetes_script.sh
+++ kubernetes_script.sh
@@ -1,1 +1,26 @@
-echo "welcome"
+echo "executing kubernetes installation script"
+sudo apt update
+curl -fsSL https://get.docker.com -oget-docker.sh
+sh get-docker.sh
+echo "enter your username"
+read username
+sudo usermod -aG docker $username
+sudo su  - <<EOF
+echo "This script is now running as a root user"
+wget https://storage.googleapis.com/golang/getgo/installer_linux
+chmod +x ./installer_linux
+./installer_linux
+source ~/.bash_profile
+go version
+git clone https://github.com/Mirantis/cri-dockerd.git
+cd cri-dockerd
+mkdir bin
+go build -o bin/cri-dockerd
+mkdir -p /usr/local/bin
+install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+cp -a packaging/systemd/* /etc/systemd/system
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
+EOF
