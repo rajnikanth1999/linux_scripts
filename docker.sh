a45f1b0dbcbceab9652d25004414d836ee8d9418
Rajnikanth1999 <praveendba31@gmail.com>
2022-12-06 15:27:39 +0530

    Docker installation script optimised

--- docker.sh
+++ docker.sh
@@ -1,14 +1,19 @@
-echo "executing docker installation script"
+echo "executing kubernetes installation script"
 sudo apt update
 status=$?
 if test $status -eq 0
 then
     curl -fsSL https://get.docker.com -oget-docker.sh
+    status=$?
 else
     echo "cant download the docker file"
+    set -e
 fi
-docker info
-status=$?
+if test $status -eq 0 
+then
+    docker info
+    status=$?
+fi
 if test $status -eq 0
 then
     echo "docker is already installed"
@@ -17,4 +22,13 @@ else
     echo "enter your username"
     read username
     sudo usermod -aG docker $username
+    echo "since the docker is installed for the first time you need to relogin"
+    exit 0
+    status=$?
+    if test $status -eq 0
+        then
+            echo "docker installation completed"
+        else
+            echo "wrong username"
+    fi
 fi
